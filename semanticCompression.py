1006b27972e12a6e46ae755e5382e03417147538
andreea <andreeaony@gmail.com>
2026-01-14 12:30:10 +0200

    Synchronized print in demo

--- semanticCompression.py
+++ semanticCompression.py
@@ -7,31 +7,31 @@ from sentence_transformers import SentenceTransformer #for generating text embed
 from sklearn.decomposition import PCA #for dimensionality reduction
 
 #dataset loading
-print("Loading dataset.csv file...")
+print("Loading dataset.csv file...", flush=True)
 dataset = pd.read_csv("dataset.csv") 
 documents = dataset['Description'].tolist()[:500] 
-print(f"Loaded {len(documents)} documents succesfully from dataset.csv.\n")
+print(f"Loaded {len(documents)} documents succesfully from dataset.csv.\n", flush=True)
 
 
 #embedding generation for full dimensions
-print("Generating full embeddings...") 
+print("Generating full embeddings...", flush=True) 
 startTime = time.time()
 model = SentenceTransformer("all-MiniLM-L6-v2") #this is a pretrained open-source model with 384-dimensional embeddings from HuggingFace
 fullEmbeddings = model.encode(documents, normalize_embeddings=True) #normalized for an easier cosine similarity calculation
 timeForFull = time.time() - startTime
 memoryForFull = fullEmbeddings.nbytes / (1024**2) #in MB
 
-print(f"Full embeddings generated in {timeForFull:.2f} seconds.\n")
-print(f"Memory used for full embeddings: {memoryForFull:.2f} MB\n")
-print(f"Full embeddings shape is {fullEmbeddings.shape}\n")
-print("Expected shape: (500, 384)\nGot shape:", fullEmbeddings.shape, "\n")
+print(f"Full embeddings generated in {timeForFull:.2f} seconds.", flush=True)
+print(f"Memory used for full embeddings: {memoryForFull:.2f} MB", flush=True)
+print(f"Full embeddings shape is {fullEmbeddings.shape}", flush=True)
+print("Expected shape: (500, 384)\nGot shape:", fullEmbeddings.shape, "\n", flush=True)
 
 
 #semantic compression using PCA 
 #PCA is am open-source linear dimensionality reduction technique that projects data onto a lower-dimensional subspace while still preserving as much variance as possible
 
 #384-dimensional embeddings are reduced to 64 dimensions
-print("Compressing embeddings from 384 dimensions to 64...")
+print("Compressing embeddings from 384 dimensions to 64...", flush=True)
 startTime = time.time()
 pca64 = PCA(n_components=64)
 compressedEmbeddings64 = pca64.fit_transform(fullEmbeddings)
@@ -39,13 +39,13 @@ compressedEmbeddings64 /= np.linalg.norm(compressedEmbeddings64, axis=1, keepdim
 timeForPCA64 = time.time() - startTime
 memoryForPCA64 = compressedEmbeddings64.nbytes / (1024**2) #in MB
 
-print(f"Compressed embeddings (64 dimensions) generated in {timeForPCA64:.2f} seconds.\n")
-print(f"Memory used for compressed embeddings (64 dimensions): {memoryForPCA64:.2f} MB\n")
-print(f"Compressed embeddings shape: {compressedEmbeddings64.shape}\n")
-print("Expected shape: (500, 64)\nGot shape:", compressedEmbeddings64.shape, "\n")
+print(f"Compressed embeddings (64 dimensions) generated in {timeForPCA64:.2f} seconds.", flush=True)
+print(f"Memory used for compressed embeddings (64 dimensions): {memoryForPCA64:.2f} MB", flush=True)
+print(f"Compressed embeddings shape: {compressedEmbeddings64.shape}", flush=True)
+print("Expected shape: (500, 64)\nGot shape:", compressedEmbeddings64.shape, "\n", flush=True)
 
 #384-dimensional embeddings are reduced to 32 dimensions
-print("Compressing embeddings from 384 dimensions to 32...")
+print("Compressing embeddings from 384 dimensions to 32...", flush=True)
 startTime = time.time()
 pca32 = PCA(n_components=32)
 compressedEmbeddings32 = pca32.fit_transform(fullEmbeddings)
@@ -53,10 +53,10 @@ compressedEmbeddings32 /= np.linalg.norm(compressedEmbeddings32, axis=1, keepdim
 timeForPCA32 = time.time() - startTime
 memoryForPCA32 = compressedEmbeddings32.nbytes / (1024**2) #in MB
 
-print(f"Compressed embeddings (32 dimensions) generated in {timeForPCA32:.2f} seconds.\n")
-print(f"Memory used for compressed embeddings (32 dimensions): {memoryForPCA32:.2f} MB\n")
-print(f"Compressed embeddings shape: {compressedEmbeddings32.shape}\n")
-print("Expected shape: (500, 32)\nGot shape:", compressedEmbeddings32.shape, "\n")
+print(f"Compressed embeddings (32 dimensions) generated in {timeForPCA32:.2f} seconds.", flush=True)
+print(f"Memory used for compressed embeddings (32 dimensions): {memoryForPCA32:.2f} MB", flush=True)
+print(f"Compressed embeddings shape: {compressedEmbeddings32.shape}", flush=True)
+print("Expected shape: (500, 32)\nGot shape:", compressedEmbeddings32.shape, "\n", flush=True)
 
 
 #semantic search function
@@ -81,26 +81,26 @@ def recall(fullResults, compressedResults):
     return recallValue
 
 #main
-query = "Students looking for affordable housing options near campus"
+query = "Schools near playgrounds with open green areas"
 
-print("Top results using FULL embeddings:")
+print("Top results using FULL embeddings:", flush=True)
 for i in search(query, fullEmbeddings):
-    print("-", documents[i][:150].replace("\n", " "))
+    print("-", documents[i][:150].replace("\n", " "), flush=True)
 
-print("\nTop results using COMPRESSED embeddings(64 dimensions):")
+print("\nTop results using COMPRESSED embeddings(64 dimensions):", flush=True)
 for i in search(query, compressedEmbeddings64, pca=pca64):
-    print("-", documents[i][:150].replace("\n", " "))
+    print("-", documents[i][:150].replace("\n", " "), flush=True)
 
-print("\nTop results using COMPRESSED embeddings(32 dimensions):")
+print("\nTop results using COMPRESSED embeddings(32 dimensions):", flush=True)
 for i in search(query, compressedEmbeddings32, pca=pca32):
-    print("-", documents[i][:150].replace("\n", " "))
+    print("-", documents[i][:150].replace("\n", " "), flush=True)
 
 recall64 = recall(search(query, fullEmbeddings), search(query, compressedEmbeddings64, pca=pca64))
 recall32 = recall(search(query, fullEmbeddings), search(query, compressedEmbeddings32, pca=pca32))
-print("\nEfficiency analysis:")
-print(f"-Full 384 dimensions - Time: {timeForFull:.2f} seconds, Memory: {memoryForFull:.2f}MB")
-print(f"-Compressed 64 dimensions - Time: {timeForPCA64:.2f} seconds, Memory: {memoryForPCA64:.2f}MB, Recall: {recall64*100:.2f}%")
-print(f"-Compressed 32 dimensions - Time: {timeForPCA32:.2f} seconds, Memory: {memoryForPCA32:.2f}MB, Recall: {recall32*100:.2f}%\n")
+print("\nEfficiency analysis:", flush=True)
+print(f"-Full 384 dimensions - Time: {timeForFull:.2f} seconds, Memory: {memoryForFull:.2f}MB", flush=True)
+print(f"-Compressed 64 dimensions - Time: {timeForPCA64:.2f} seconds, Memory: {memoryForPCA64:.2f}MB, Recall: {recall64*100:.2f}%", flush=True)
+print(f"-Compressed 32 dimensions - Time: {timeForPCA32:.2f} seconds, Memory: {memoryForPCA32:.2f}MB, Recall: {recall32*100:.2f}%\n", flush=True)
 
 #plotting time, memory and recall
 labels = ['Full 384 dimensions', 'Compressed 64 dimensions', 'Compressed 32 dimensions']
